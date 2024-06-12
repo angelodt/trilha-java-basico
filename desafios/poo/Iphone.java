226d8d75e82c6f1a95dd321fe6c52658da95523d
angelodt <32304707+angelodt@users.noreply.github.com>
2024-06-12 11:34:22 -0300

    Create Iphone.java
    feat: entrega desafio trilha java básico poo add Iphone.java

---
+++ desafios/poo/Iphone.java
@@ -0,0 +1,41 @@
+public class Iphone {
+
+  public class ReprodutorMusical {
+    public void tocar() {
+
+    }
+    public void pausar() {
+
+    }
+
+    public void selecionarMusica(String musica) {
+
+    }
+  }
+
+  public class AparelhoTelefonico {
+    public void atender() {
+
+    }
+    public void iniciarCorreioVoz() {
+
+    }
+
+    public void ligar(String numero) {
+
+    }
+  }
+
+ public class NavegadorInternet {
+    public void atualizarPagina() {
+
+    }
+    public void adicionarNovaAba() {
+
+    }
+
+    public void exibirPagina(String url) {
+
+    }
+  }
+}
